24d40039d9503484ae5af2dedf9c2270b71b6337
pyyaoer <pyyaoer@gmail.com>
2016-01-05 21:02:47 +0800

    generate header via MIMEText

--- Useful_Parts/python/mail_sender.py
+++ Useful_Parts/python/mail_sender.py
@@ -1,7 +1,9 @@
 import smtplib
+from email.mime.text import MIMEText
 
-def sendmail(from_addr, to_addr_list, cc_addr_list, subject, message, login, password, smtpserver='smtp.163.com'):
-
+# Naive version
+def sendmail_0(from_addr, to_addr_list, cc_addr_list, subject, message, login, password, smtpserver='smtp.163.com'):
+        'Naive version'
 	header  = 'From: %s\n' % from_addr
 	header += 'To: %s\n' % ','.join(to_addr_list)
 	header += 'Cc: %s\n' % ','.join(cc_addr_list)
@@ -11,12 +13,28 @@ def sendmail(from_addr, to_addr_list, cc_addr_list, subject, message, login, pas
 	server = smtplib.SMTP(smtpserver)
 	server.starttls()
 	server.login(login, password)
-	problems = server.sendmail(from_addr, to_addr_list, message)
+	problems = server.sendmail(from_addr, to_addr_list + cc_addr_list, message)
 	server.quit()
 	return problems
 
+# Simple text email, generate header via MIMEText
+def sendmail_1(from_addr, to_addr_list, cc_addr_list, subject, message, login, password, smtpserver='smtp.163.com'):
+        'Simple text email, generate header via MIMEText'
+        msg = MIMEText(message)
+        msg['Subject'] = subject
+        msg['From'] = from_addr
+        msg['To'] = ','.join(to_addr_list)
+        msg['Cc'] = ','.join(cc_addr_list)
 
-sendmail(from_addr = '',
+        server = smtplib.SMTP(smtpserver)
+	server.starttls()
+	server.login(login, password)
+        problems = server.sendmail(from_addr, to_addr_list + cc_addr_list, msg.as_string())
+        server.quit()
+        return problems
+
+
+sendmail_1(from_addr = '',
 		to_addr_list = [],
 		cc_addr_list = [],
 		subject = 'test',
